95d4eb6cd1231ddddb78c00ca2f7b4b0861cb2b3
Chris Cannam <cannam@all-day-breakfast.com>
2013-01-04 14:36:02 +0000

    Construct caches

--- src/AudioStreamColumnReader.cpp
+++ src/AudioStreamColumnReader.cpp
@@ -6,6 +6,10 @@
 #include "base/DataAreaCache.h"
 #include "base/DataValueCache.h"
 #include "base/TurbotTypes.h"
+#include "base/Exceptions.h"
+
+#include "AudioReadStream.h"
+#include "AudioReadStreamFactory.h"
 
 namespace Turbot {
 
@@ -13,6 +17,8 @@ class AudioStreamColumnReader::D
 {
 public:
     D(QString filename) :
+	m_filename(filename),
+	m_stream(0),
 	m_channels(0),
 	m_columnCache(0),
 	m_streamCache(0)
@@ -22,9 +28,37 @@ public:
     ~D() {
     }
 
-    void open();
+    void open() {
+	if (m_stream) {
+	    throw PreconditionFailed("AudioStreamColumnReader already open");
+	}
 
-    QString getFileName() const;
+	m_stream = AudioReadStreamFactory::createReadStream(m_filename);
+	m_channels = m_stream->getChannelCount();
+	m_timebase = Timebase(m_stream->getSampleRate(),
+			      DefaultColumnSize,
+			      DefaultHopSize);
+
+        // Somewhat arbitrary cache size of 8 columns across all
+        // channels. We need to allow backward seeks of at least 1 or
+        // 2 columns for Region to operate.
+	m_columnCache = new DataValueCache<turbot_sample_t>
+	    (8 * m_channels * getHeight());
+
+        m_streamCache = new DataValueCache<float> *[m_channels];
+
+        for (int c = 0; c < m_channels; ++c) {
+            // Stream cache is exactly one FFT-input frame in size,
+            // and we read a hop then push it on the end at each step
+            m_streamCache[c] = new DataValueCache<float>
+                (m_timebase.getColumnSize());
+        }
+    }
+
+
+    QString getFileName() const {
+	return m_filename;
+    }
 
     Timebase getTimebase() const {
 	return m_timebase;
@@ -45,14 +79,23 @@ public:
     bool getColumnPolarInterleaved
     (int x, int channel, turbot_sample_t *column);
 
-    void close();
-
+    void close() {
+        for (int c = 0; c < m_channels; ++c) {
+            delete m_streamCache[c];
+        }
+	delete[] m_streamCache;
+	delete m_columnCache;
+	delete m_stream;
+	m_stream = 0;
+    }
 
 private:
+    QString m_filename;
+    AudioReadStream *m_stream;
     int m_channels;
     Timebase m_timebase;
-    DataValueCache<turbot_sample_t> *m_columnCache;
-    DataValueCache<float> *m_streamCache;
+    DataValueCache<turbot_sample_t> *m_columnCache; // columns interleaved
+    DataValueCache<float> **m_streamCache; // per channel
 };
 
 AudioStreamColumnReader::AudioStreamColumnReader(QString filename) :
