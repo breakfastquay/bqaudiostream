af663ce7a5b1e414be060372a80a72a4427febbf
Chris Cannam <cannam@all-day-breakfast.com>
2013-01-05 09:36:08 +0000

    Start audiostream tests

---
+++ src/test/main.cpp
@@ -0,0 +1,30 @@
+/* -*- c-basic-offset: 4 indent-tabs-mode: nil -*-  vi:set ts=8 sts=4 sw=4: */
+/* Copyright Chris Cannam - All Rights Reserved */
+
+#include "TestSimpleWavRead.h"
+//#include "TestAudioStreamColumnReader.h"
+#include <QtTest>
+
+#include <iostream>
+
+int main(int argc, char *argv[])
+{
+    int good = 0, bad = 0;
+
+    QCoreApplication app(argc, argv);
+
+    {
+	Turbot::TestSimpleWavRead t;
+	if (QTest::qExec(&t, argc, argv) == 0) ++good;
+	else ++bad;
+    }
+
+    if (bad > 0) {
+	std::cerr << "\n********* " << bad << " test suite(s) failed!\n" << std::endl;
+	return 1;
+    } else {
+        std::cerr << "All tests passed" << std::endl;
+        return 0;
+    }
+}
+
